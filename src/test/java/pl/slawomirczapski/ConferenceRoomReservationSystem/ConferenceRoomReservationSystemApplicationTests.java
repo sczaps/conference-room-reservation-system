96118f52755122206e7d20cf65c702a818120de1
SlawomirCzapski <czapski.slawomir@gmail.com>
2022-07-30 12:25:03 +0200

    Init ConferenceRoomReservationSystem project

---
+++ src/test/java/pl/slawomirczapski/ConferenceRoomReservationSystem/ConferenceRoomReservationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.slawomirczapski.ConferenceRoomReservationSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConferenceRoomReservationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
